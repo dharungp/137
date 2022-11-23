45ba40f55412388632ee3569462a7a1edcd39877
dharungp <99897041+dharungp@users.noreply.github.com>
2022-11-23 19:37:03 +0530

    Add files via upload

---
+++ Bedroom.js
@@ -0,0 +1,13 @@
+img="";
+function Back(){
+    window.location="index.html"
+}
+
+function preload(){
+    img = loadImage('bedroom.jpg');
+}
+
+function setup(){
+    canvas = createCanvas(640,420);
+    canvas.center();
+    }
---
+++ main.js
@@ -0,0 +1,20 @@
+img=""
+function Bedroom(){
+    window.location="Bedroom.html"
+}
+
+function TvAc(){
+    window.location="TvAc.html"
+}
+
+function Hall(){
+    window.location="hall.html"
+}
+
+function Fruit(){
+    window.location="Fruit.html"
+}
+
+function Garden(){
+    window.location="Graden.html"
+}
